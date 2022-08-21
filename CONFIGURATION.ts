ee4c3805ff3c953280b9532b11459aeedee8cba0
Mushon Zer-Aviv <mushon@shual.com>
2022-08-21 12:07:29 +0300

    Atlas colors

--- CONFIGURATION.ts
+++ CONFIGURATION.ts
@@ -8,5 +8,5 @@ export const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiYXRsYXNtZWRsaXEiLCJhIjoiY2tpbXgzN
 
 export const HORIZONTAL_LAYOUT = true; // enable for horizontal layout
 
-export const PRIMARY_COLOR = '#0085FF'; // #CE0031
-// export const PRIMARY_COLOR = '#6E0023';
+export const PRIMARY_COLOR = '#00A881'; // #CE0031
+export const SECONDARY_COLOR = '#004233';
