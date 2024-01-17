887b2349998ea763779dd26032081af606995bcf
Adam Kariv <akariv@users.noreply.github.com>
2024-01-17 16:52:42 +0200

    Update auth token

--- CONFIGURATION.ts
+++ CONFIGURATION.ts
@@ -1,2 +1,2 @@
 export const BASEROW_ENDPOINT = 'https://manage.chronomaps.net';
-export const BASEROW_ADMIN_TOKEN = 'oNDzIeOPHd6bdk38uGMTs7l7wS5qJcXh';
+export const BASEROW_ADMIN_TOKEN = 'M848mzHlYh36iWPJ1Y9PxxxIburb5z5z';
