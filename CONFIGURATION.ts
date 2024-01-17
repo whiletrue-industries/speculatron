fe6199397c88e369fc998b391c044fe5ff74a4f0
Adam Kariv <adam.kariv@gmail.com>
2024-01-17 09:58:56 +0200

    Move to new website

--- CONFIGURATION.ts
+++ CONFIGURATION.ts
@@ -1,3 +1,2 @@
-export const BASEROW_ENDPOINT = 'https://baserow.datacity.org.il';
-export const BASEROW_ADMIN_DB = 110;
-export const BASEROW_ADMIN_TOKEN = 'hgGDtvhhPavOCwtGmbv7ANaXc4UQdq6l';
+export const BASEROW_ENDPOINT = 'https://manage.chronomaps.net';
+export const BASEROW_ADMIN_TOKEN = 'oNDzIeOPHd6bdk38uGMTs7l7wS5qJcXh';
